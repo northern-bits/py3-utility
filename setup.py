6a63c1151ff82bc9cb9944d3218e315ee50c48e5
rdgl <ryndgl@pm.me>
2022-06-17 14:09:00 -0400

    fixed install requires

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
+from pkg_resources import parse_requirements
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 with open('requirements.txt', 'r',  encoding='utf-8') as f:
-    requirements = [x for x in f.read()]
+    requirements = [x for x in parse_requirements(f.read())]
+
 setup(
     name='py3-utility',
     version='1.0',
